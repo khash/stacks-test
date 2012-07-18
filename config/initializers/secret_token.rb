1c189d91f26e9c4ad005954225018bbee4f0209d
Khash Sajadi <khash@sajadi.co.uk>
2012-07-18 10:32:18 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stacks::Application.config.secret_token = 'bf775541d3682879766df9283131e2a4a1c49ab1a0ac5dece69fe36c3363d24762ddea2c0cd79dba0294d32716114bac09e36ea581deec636c679297c5d5e095'
